99e58b3b1ea5b4c30d9bba340f9f3065bab21e85
Dylan Harness <dharness@uwo.ca>
2017-01-19 12:42:19 -0500

    basic hookup of feature extractor via API call

--- api/app/api/uploadController.js
+++ api/app/api/uploadController.js
@@ -2,25 +2,50 @@ const uploadController = require('express').Router();
 const formidable = require('formidable');
 const path = require('path');
 const fs = require('fs');
+const featureExtractor = require('./../services/featureExtractor');
 
 uploadController.get('/', (req, res) => {
     res.render('upload')
 });
 
 uploadController.post('/', function(req, res){
-  console.log(req.body);
-  var form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm();
   form.multiples = true;
   form.uploadDir = path.join(__dirname, '/../uploads');
-  form.on('file', function(field, file) {
-    fs.rename(file.path, path.join(form.uploadDir, file.name));
+
+  const readStream = fs.createReadStream(path.join(__dirname, './../uploads/audio.wav'));
+  const writeStream = featureExtractor.extract((featureVector) => {
+    res.send(featureVector);
   });
-  form.on('error', function(err) {
-    console.log('An error has occured: \n' + err);
-  });
-  form.on('end', function() {
-    res.end('success');
-  });
+  form.onPart = function(part) {
+    if (!part.filename) {
+        // let formidable handle all non-file parts
+        form.handlePart(part);
+        return;
+    }
+    part.on('data', (chunk) => {
+        writeStream.write({ data: new Uint8Array(chunk) });
+    });
+    part.on('end', () => {
+      writeStream.end();
+    });
+    part.on('error', function(err) {
+        // handle this too
+    });
+  }
+
+  // form.on('file', (field, file) => {
+  //   res.send(200);
+  //   fs.rename(file.path, path.join(form.uploadDir, file.name));
+  //   console.log(readStream)
+  // });
+
+  // form.on('error', (err) => {
+  //   console.log('An error has occured: \n' + err);
+  // });
+
+  // form.on('end', () => res.end('success'));
+
   form.parse(req);
 });
 
