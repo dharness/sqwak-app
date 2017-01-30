f5b68e7b263099495b39d04fe624cd129eda2c5c
Dylan Harness <dharness@uwo.ca>
2017-01-29 22:50:40 -0500

    added some disabled state checking etc

--- web/app/src/components/shared/ClassUploadForm.js
+++ web/app/src/components/shared/ClassUploadForm.js
@@ -9,7 +9,8 @@ class ClassUploadForm extends Component {
   constructor() {
     super();
     this.state = {
-      files: []
+      files: [],
+      className: ""
     }
   }
 
@@ -22,8 +23,13 @@ class ClassUploadForm extends Component {
         console.log(res.srcElement.responseText)
     });
   }
+
   onFilesChanged() {
     this.setState({files: this.fileInput.files});
+  }
+  
+  handleClassNameChange(event) {
+    this.setState({className: event.target.value});
   }
 
   browse() {
@@ -31,12 +37,21 @@ class ClassUploadForm extends Component {
   }
 
   render () {
+    let formIsValid = (this.state.files.length > 0 && this.state.className !== "");
     return (
       <div className="sq-class-upload-form">
         <div className="sq-class-upload-form--container">
           <div className="sq-text__xl sq-class-upload-form--title">Create Class</div>
-          <input type="text" className="sq-basic-input" placeholder="class name"/>
-          <div className="sq-class-upload-form--drop-zone">
+          <input
+            type="text"
+            className="sq-basic-input"
+            value={this.state.className}
+            onChange={this.handleClassNameChange.bind(this)}
+            placeholder="class name"
+          />
+          <div className={"sq-class-upload-form--drop-zone" +
+            (this.state.files.length <= 0 ? "" : " active")}
+            >
             <img src={fileUploadImg} role="presentation" className="sq-class-upload-form--icon"/>
             <div className="sq-class-upload-form--sub-1 sq-text__lg">Drag & drop</div>
             <div className="sq-class-upload-form--sub-2 sq-text__pale">
@@ -52,11 +67,16 @@ class ClassUploadForm extends Component {
                   onChange={this.onFilesChanged.bind(this)}
                   multiple="multiple"/>
             </div>
+            <div
+                style={{display: (this.state.files.length <= 0) ? "none" : "block"}}
+              className="sq-class-upload-form--upload-message sq-text__lg">
+              {this.state.files.length} file ready to upload!
+            </div>
           </div>
           <PlushButton 
             buttonText="Creat class" 
             onClick={this.uploadAudio.bind(this)} 
-            disabled={this.state.files.length <= 0}/>
+            disabled={!formIsValid}/>
         </div>
       </div>
     )
