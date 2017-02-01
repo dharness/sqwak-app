18a0b391b75d64e20218a249d3a1a8fa6e331c01
Dylan Harness <dharness@uwo.ca>
2017-01-31 21:50:20 -0500

    added delete class;

--- web/app/src/actions/mlClasses.js
+++ web/app/src/actions/mlClasses.js
@@ -1,14 +1,31 @@
-import { createClass } from './../services/api';
+import { createClass, deleteClass } from './../services/api';
 
 
 export const createMlClass = (mlClassData) => {
   return (dispatch) => {
-    createClass(mlClassData).then((res) => {
+    createClass(mlClassData).then( res => {
       const mlClass = JSON.parse(res.srcElement.responseText);
       dispatch({
         type: 'ADD_ML_CLASS',
         mlAppId: mlClassData.appId,
         mlClass
+      });
+      dispatch({ type: 'CLOSE_MODAL' });
+    });
+  };
+}
+
+export const deleteMlClass = ({mlAppId, mlClassId}) => {
+  return (dispatch) => {
+    const mlClassData = {
+      appId: mlAppId,
+      classId: mlClassId
+    };
+    deleteClass(mlClassData).then( res => {
+      dispatch({
+        type: 'REMOVE_ML_CLASS',
+        mlAppId,
+        mlClassId
       });
       dispatch({ type: 'CLOSE_MODAL' });
     });
