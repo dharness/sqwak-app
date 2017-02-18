9e976bb7bbd4d5543c989fb3145d4dd9dfd64aba
nnur <23naila.n@gmail.com>
2017-02-17 23:39:15 -0500

    lots of style stuff

--- web/app/src/components/pages/Landing.js
+++ web/app/src/components/pages/Landing.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router'
+import { browserHistory } from 'react-router';
 import giantBird from './../../assets/images/giant-bird.svg'
 import sqwakWord from './../../assets/images/sqwak-word.svg'
 import googleImg from './../../assets/images/google.svg'
@@ -10,10 +11,16 @@ import kotsLogo from './../../assets/images/kots-logo.svg'
 import emailIcon from './../../assets/images/mail.svg'
 import fbIcon from './../../assets/images/fblittle.svg'
 import LandingPreviewSlider from './../shared/LandingPreviewSlider';
+import PlushButton from './../shared/PlushButton';
 
 
 
 class LandingPage extends Component {
+
+      go(route) {
+        browserHistory.push(route);
+      }
+
     render () {
         return (
             <div className="sq-landing">
@@ -21,11 +28,11 @@ class LandingPage extends Component {
                 {/* HEADING */}
                 <div className="sq-landing--above-fold">
                     <div className="sq-landing--nav">
-                        <Link to="/login" className="sq-landing--login-link">
-                            Login
+                        <Link to="/login" className="sq-text__thick sq-landing--login-link">
+                            Log In
                         </Link>
-                        <Link to="/signup" className="sq-landing--signup-link">
-                            Signup
+                        <Link to="/signup" className="sq-text__thick sq-landing--signup-link">
+                            Sign Up
                         </Link>
                     </div>
                     <div className="sq-landing--hero">
@@ -34,10 +41,12 @@ class LandingPage extends Component {
                         </div>
                         <div className="sq-landing--hero-info">
                             <img src={sqwakWord} role="presentation" className="sq-landing--title"/>
-                            <p className="sq-landing--prompt">Teach your apps to listen with your personal machine learning dashboard</p>
-                            <Link to="/signup" className="sq-landing--signup-link">
-                                Try it free!
-                            </Link>
+                            <p className="sq-text__thick sq-landing--prompt">Teach your apps to hear with our simple machine learning dashboard</p>
+                            <PlushButton
+                                buttonText="Try it free!" 
+                                onClick={()=>{this.go('/signup')}}
+                                colorClass="sq-button__red"
+                            />
                         </div>
                     </div>
                 </div>
@@ -55,7 +64,7 @@ class LandingPage extends Component {
                     <div className="sq-landing--partners">
                         <div className="sq-landing--partners--header">
                             <div className="sq-text sq-text__xl">Partners and clients</div>
-                            <div className="sq-text sq-text__lg">we wish we had...</div>
+                            <div className="sq-text sq-text__lg">we wish we had</div>
                         </div>
                         <div className="sq-landing--partner-list">
                             <div className="sq-landing--partner-list-content">
@@ -66,9 +75,9 @@ class LandingPage extends Component {
                             </div>
                         </div>
                         <div className="sq-landing--partners--footer">
-                            <div className="sq-text sq-text__xl">Start using Sqwak today for free!</div>
-                            <Link to="/signup" className="sq-landing--signup-link">
-                                Create a Free Account
+                            <div className="sq-text sq-text__lg">Start using Sqwak today for free!</div>
+                            <Link to="/signup" className="sq-text__thick sq-landing--signup-link">
+                                Create Free Account
                             </Link>
                         </div>
                     </div>
@@ -83,13 +92,13 @@ class LandingPage extends Component {
                             <hr/>
                         </div>
                         <div className="sq-landing--footer-company">
-                            <div className="sq-text sq-text__xl">Company</div>
-                            <a href="https://kingofthestack.com/" className="sq-text">About</a>
-                            <a href="https://kingofthestack.com/portfolio.html" className="sq-text">Portfolio</a>
-                            <a href="https://kingofthestack.com/learn.html" className="sq-text">Teaching</a>
+                            <div className="sq-landing--footer-header sq-text__thick">Company</div>
+                            <a href="https://kingofthestack.com/">About</a>
+                            <a href="https://kingofthestack.com/portfolio.html">Portfolio</a>
+                            <a href="https://kingofthestack.com/learn.html">Teaching</a>
                         </div>
                         <div className="sq-landing--footer-contact">
-                            <div className="sq-text sq-text__xl">Contact</div>
+                            <div className="sq-landing--footer-header sq-text__thick">Contact</div>
                             <div className="sq-landing--footer-address">
                                 715 Bryant st. <br/>
                                 Toronto, Ontario <br/>
@@ -110,21 +119,25 @@ class LandingPage extends Component {
                         </div>
                         <div className="sq-landing--footer-kots">
                             <img src={kotsLogo} role="presentation"/>
-                            <div className="sq-text sq-text__lg">A King of the Stack product</div>
+                            <div className="sq-text__lg">A King of the Stack product</div>
                             <a href="mailto:hello@kingofthestack.com">hello@kingofthestack.com</a>
                             <hr/>
                         </div>
                     </div>
 
-                    <div className="sq-landing--footer-footer">
+                    <div className="sq-text__md sq-landing--footer-footer">
                         <p>
                             Copyright 2017 King of the Stack Inc.
                             <br/>
                             All rights reserved
                             <br/>
+                        </p>
+                        <p> 
+                            Privacy Policy & Terms of Service 
+                            <br/>
                             🇨🇦 made with Canadian maple syrup
                         </p>
-                        <p> Privacy Policy & Terms of Service </p>
+                            
                     </div>
                 </div>
             </div>
