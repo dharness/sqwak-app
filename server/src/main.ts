2242b217efe7b2dc2c0d86aa277171596d7dc804
Dylan Harness <dylan@NailasMacStudio.attlocal.net>
2023-07-30 19:31:41 -0700

    .

--- server/src/main.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv-flow";
-dotenv.config({ silent: false });
-import app from "./app";
-
-const port = process.env.PORT || 5050;
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
